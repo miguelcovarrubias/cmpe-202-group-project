3d3db20bbfe21013e519f933cb20ca6fa29a1ceb
maninderjits <maninderjit5embhy@hotmail.com>
2019-05-04 23:52:32 -0700

    some startup code

---
+++ home.php
@@ -0,0 +1,8 @@
+<?php 
+	//include "createUser.php";
+	$generic_path = "/var/www/html/cmpe-202-group-project/";
+	echo "<h1> This is the home page </h1> <br>";
+	//Below I am combining the generic path variable with the rest of the string       
+?>
+
+<a href = "cmpe-202-group-project/createUser.php" > Sign Up </a> <br>
