cc475e9c78e08f1f0c467f89aec1ae8f5a51562c
maninderjits <maninderjit5embhy@hotmail.com>
2019-05-05 17:31:24 -0700

    Just changed something small in home.php

--- home.php
+++ home.php
@@ -1,8 +1,18 @@
 <?php 
-	//include "createUser.php";
 	$generic_path = "/var/www/html/cmpe-202-group-project/";
-	echo "<h1> This is the home page </h1> <br>";
-	//Below I am combining the generic path variable with the rest of the string       
+        echo "<h1> This is the home page </h1> <br>";
+	//include "createUser.php";
+	session_start();
+
+	if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == TRUE){
+		echo "You are logged in. <br>";
+		echo "<a href = 'cmpe-202-group-project/php_card_starter/logout.php' > Logout </a> <br>";
+	}
+	else {
+		echo "<a href = 'cmpe-202-group-project/php_card_starter/login.php' > Login </a> <br>";
+		echo "<a href = 'cmpe-202-group-project/php_card_starter/register.php' > Sign Up </a> <br>";
+	
+	}
+	
 ?>
 
-<a href = "cmpe-202-group-project/createUser.php" > Sign Up </a> <br>
